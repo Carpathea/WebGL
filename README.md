c690013736229149e44f49b5d08805c74457696c
Thomas Brown <thomas.a.brown@hotmail.co.uk>
2019-08-20 09:42:56 +0100

    using 3.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebGL
 Investigation into WebGL as part of persuing my interest in 3D programming.
+
+# Motivation
+The motivation behind the project is to find any libraries or external tools to create terrains in 3D.
+I discovered tutorials using three.js found here https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
+Full credit to the creators.
--- WebGL/wwwroot/js/site.js
+++ WebGL/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(document).ready(function () {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+});
